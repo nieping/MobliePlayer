844a0fb72d5385ca248adbffc2944e6f3fee8619
nieping <nieping1991>
2018-05-13 18:11:34 +0800

    整合

--- app/src/main/java/com/example/administrator/moblieplayer/view/fragment/LocalMusicFragment.java
+++ app/src/main/java/com/example/administrator/moblieplayer/view/fragment/LocalMusicFragment.java
@@ -1,7 +1,12 @@
 package com.example.administrator.moblieplayer.view.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.administrator.moblieplayer.R;
 import com.example.administrator.moblieplayer.view.base.BaseFragment;
 
 /**
@@ -9,9 +14,10 @@ import com.example.administrator.moblieplayer.view.base.BaseFragment;
  */
 
 public class LocalMusicFragment extends BaseFragment {
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view inflater.inflate(R.layout.fragment_localmusic)
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
--- app/src/main/java/com/example/administrator/moblieplayer/view/fragment/LocalVideoFragment.java
+++ app/src/main/java/com/example/administrator/moblieplayer/view/fragment/LocalVideoFragment.java
@@ -1,17 +1,26 @@
 package com.example.administrator.moblieplayer.view.fragment;
 
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.example.administrator.moblieplayer.view.base.BaseFragment;
+import com.example.administrator.moblieplayer.R;
 
 /**
  * Created by Administrator on 2018/5/13.
  */
 
-public class LocalVideoFragment extends BaseFragment {
+public class LocalVideoFragment extends Fragment {
+    private Context mContext;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_localvideo,container,false);
+        return view;
+
     }
 }
